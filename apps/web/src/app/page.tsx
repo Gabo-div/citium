4768b9afb1202fcca7bf2bbaebf1837e897b292e
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-09-19 15:30:41 -0400

    feat(apps/web): seccion de proyectos

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,12 +2,14 @@ import Contact from "@/components/layout/Contact";
 import Footer from "@/components/layout/Footer";
 import Hero from "@/components/layout/Hero";
 import Services from "@/components/layout/Services";
+import Projects from "@/components/layout/Projects";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
+      <Projects />
       <Contact />
       <Footer />
     </>
