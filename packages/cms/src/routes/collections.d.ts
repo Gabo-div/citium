0e95d234c743f0703b6259810884d468c10ae3b2
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-10-08 21:20:47 -0400

    chore(packages/cms): eliminar archivos de declaracion en src

--- packages/cms/src/routes/collections.d.ts
+++
@@ -1,3 +0,0 @@
-import { Hono } from "hono";
-import { Adapter } from "@citium/types/adapter";
-export declare const getCollectionsRoutes: (adapter: Adapter) => Hono<import("hono/types").BlankEnv, import("hono/types").BlankSchema, "/">;
