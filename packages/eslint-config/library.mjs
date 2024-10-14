086e2222ce49923a8a907a91e019719600b1390e
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-10-14 16:11:20 -0400

    chore(packages/eslint-config): errores de dependencias

--- packages/eslint-config/library.mjs
+++ packages/eslint-config/library.mjs
@@ -2,8 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
+  baseDirectory: __dirname,
   recommendedConfig: pluginJs.configs.recommended,
   allConfig: pluginJs.configs.all,
 });
--- packages/eslint-config/react.mjs
+++ packages/eslint-config/react.mjs
@@ -3,8 +3,13 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
+  baseDirectory: __dirname,
   recommendedConfig: pluginJs.configs.recommended,
   allConfig: pluginJs.configs.all,
 });
